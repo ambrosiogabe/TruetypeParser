7e6ea32d94600bb6d36242f552c0571bef287a3a
Gabe Ambrosio <ambrosiogabe@gmail.com>
2021-01-08 11:09:33 -0600

    Refactor truetype into its own folder

---
+++ Truetype/premake5.lua
@@ -0,0 +1,52 @@
+project "Truetype"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+
+    targetdir ("../bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("../bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+        "include/**.h",
+		"cpp/**.cpp",
+	}
+
+    disablewarnings { 
+        "4251" 
+    }
+
+	defines {
+        --"_CRT_SECURE_NO_WARNINGS"
+	}
+
+	includedirs {
+        "include",
+	}
+
+	links {
+		--"GLFW",
+        "opengl32.lib",
+	}
+
+    filter { "system:windows", "configurations:Debug" }
+        buildoptions "/MTd"        
+
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
+
+	filter "system:windows"
+		systemversion "latest"
+    
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+
+	filter "configurations:Dist"
+		runtime "Release"
+        optimize "on"
